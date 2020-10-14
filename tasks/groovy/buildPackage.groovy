09f97c520bb242592ec7f2c8e9ee21bf859d357a
sethlinnpubliq <59605201+sethlinnpubliq@users.noreply.github.com>
2020-10-14 09:44:25 +0200

    Create buildPackage.groovy

---
+++ tasks/groovy/buildPackage.groovy
@@ -0,0 +1,16 @@
+//This script is intended to be called from repoName/tasks/groovy/
+def call() {
+   println "Creating a debian package from the binaries."
+   "ls -a".execute()
+   def currentDir = new File( "." ).getCanonicalPath()
+   println "Current dir:" + currentDir
+   
+   def upTwo = new File( "../../" ).getCanonicalPath()
+   println "UpTwo dir:" + upTwo
+   
+   dir('../../') {
+      def currentDir2 = new File( "." ).getCanonicalPath()
+      println "Current dir2:" + currentDir2
+   }
+   //command.execute(null, new File("Working dir"))
+}

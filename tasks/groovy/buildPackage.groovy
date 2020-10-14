e61113b92977e3e1da483aa580303928c9e45d61
sethlinnpubliq <59605201+sethlinnpubliq@users.noreply.github.com>
2020-10-14 18:36:07 +0200

    Update buildPackage.groovy

--- tasks/groovy/buildPackage.groovy
+++ tasks/groovy/buildPackage.groovy
@@ -1,4 +1,4 @@
-//This script is intended to be called from repoName/tasks/groovy/
+//This script is intended to be called from the repo root.
 //def call() {
    println "Creating a debian package from the binaries."
    //echo "Blah Blah"
@@ -8,7 +8,18 @@
    
    def upTwo = new File( "../../" ).getCanonicalPath()
    println "UpTwo dir:" + upTwo
-   
-   //command.execute(null, new File("Working dir"))
+
+String text = '''{\n
+"publicPath": "PLACEHOLDER_PUBLICPATH",\n
+"apiUrl": "PLACEHOLDER_APIURL",\n
+"baseUrl" : "PLACEHOLDER_BASEURL",\n
+"title": "PLACEHOLDER_TITLE"\n
+}'''
+
+File file = new File("config/config.json")
+file.append(text)
+println file.text
+
+
 //}
 return this

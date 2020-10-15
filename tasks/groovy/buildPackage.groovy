d49ae632276e6d647637e21891fdc421079a91f0
sethlinnpubliq <59605201+sethlinnpubliq@users.noreply.github.com>
2020-10-15 08:42:48 +0200

    Update buildPackage.groovy

--- tasks/groovy/buildPackage.groovy
+++ tasks/groovy/buildPackage.groovy
@@ -1,25 +1,35 @@
-//This script is intended to be called from the repo root.
-//def call() {
-   println "Creating a debian package from the binaries."
-   //echo "Blah Blah"
-   "ls -a".execute()
-   def currentDir = new File( "." ).getCanonicalPath()
-   println "Current dir:" + currentDir
-   
-   def upTwo = new File( "../../" ).getCanonicalPath()
-   println "UpTwo dir:" + upTwo
+// This script is intended to be called from the repo root.
+// Prerequisites:
+// Groovy installed(latest versio is ok)
+// Node.js version 6.11.1 installed
+// Bundler installed
+// Run: bundler
+// 
 
-String text = '''{\n
-"publicPath": "PLACEHOLDER_PUBLICPATH",\n
-"apiUrl": "PLACEHOLDER_APIURL",\n
-"baseUrl" : "PLACEHOLDER_BASEURL",\n
-"title": "PLACEHOLDER_TITLE"\n
+println "Creating a debian package from the binaries."
+
+def currentDir = new File( "." ).getCanonicalPath()
+println "Current dir:" + currentDir
+
+// File content
+//String text = '''{\n
+//"publicPath": "PLACEHOLDER_PUBLICPATH",\n
+//"apiUrl": "PLACEHOLDER_APIURL",\n
+//"baseUrl" : "PLACEHOLDER_BASEURL",\n
+//"title": "PLACEHOLDER_TITLE"\n
+//}'''
+
+//Create new dir and file with content, 
+File dir = new File('config')
+dir.mkdir();
+File file = new File(dir + "config.json")
+file.createNewFile()
+file.text = '''{
+"publicPath": "PLACEHOLDER_PUBLICPATH",
+"apiUrl": "PLACEHOLDER_APIURL",
+"baseUrl" : "PLACEHOLDER_BASEURL",
+"title": "PLACEHOLDER_TITLE"
 }'''
-
-File file = new File("config/config.json")
-file.append(text)
 println file.text
 
-
-//}
 return this

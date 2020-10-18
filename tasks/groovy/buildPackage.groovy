7e4c20ab352f2734404592c8ef4783c2410fc4a2
sethlinnpubliq <59605201+sethlinnpubliq@users.noreply.github.com>
2020-10-18 16:39:33 +0200

    Update buildPackage.groovy

--- tasks/groovy/buildPackage.groovy
+++ tasks/groovy/buildPackage.groovy
@@ -71,8 +71,8 @@ def dir = new File("pkg").mkdirs()
 
 String command = '''bundle exec fpm -t deb -n groepspas-angular-app -v ''' + fileName + 
                  ''' -s dir -a all -p pkg --deb-user www-data --deb-group www-data''' + 
-                 ''' --license "Apache-2.0" -m "Infra publiq"''' +
-                 ''' --url "dude" --vendor "publiq vzw"''' +
+                 ''' --license "Apache-2.0" -m "Infra dude"''' +
+                 ''' --url "dude" --vendor "dude vzw"''' +
                  ''' --description "AngularJS frontend for Groepspas"''' + 
                  ''' --prefix /var/www/groepspas --before-remove prerm -C dist''' + 
                  ''' -d rubygem-angular-config .'''

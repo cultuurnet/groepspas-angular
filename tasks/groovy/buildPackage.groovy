4ca8b26358b7db52c6e785bd0efd5c5e3083c43b
sethlinnpubliq <59605201+sethlinnpubliq@users.noreply.github.com>
2020-10-19 11:11:14 +0200

    Update buildPackage.groovy

--- tasks/groovy/buildPackage.groovy
+++ tasks/groovy/buildPackage.groovy
@@ -89,7 +89,7 @@ def dir = new File("pkg").mkdirs()
 List command = ['bundle', 'exec', 'fpm', '-t', 'deb', '-n', 'groepspas-angular-app', '-v', "${fileName}", '-s', 'dir', '-a', 'all', 
                 '-p', 'pkg', '--deb-user', 'www-data', '--deb-group', 'www-data', '--license', '\"Apache-2.0\"',
                 '-m', 'Infra publiq <infra@publiq.be>', '--url', '''"https://www.publiq.be"''', '--vendor', 'publiq vzw',
-                '--description', '\"AngularJS frontend for Groepspas\"', '--prefix', '''/var/www/groepspas''' '--before-remove', 'prerm', 
+                '--description', '\"AngularJS frontend for Groepspas\"', '--prefix', '''/var/www/groepspas''', '--before-remove', 'prerm', 
                 '-C', 'dist', '-d', 'rubygem-angular-config', '.']
 
 runCommand command
